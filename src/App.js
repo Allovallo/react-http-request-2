c43efabab6188cd31d1682ce68743d900db0feeb
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-06-02 14:20:27 +0300

    files updated_tutorial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div>App</div>;
-}
+import React, { Component } from 'react';
 
-export default App;
+export default class App extends Component {
+  state = {
+    pockemon: null,
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    fetch('http://pokeapi.co/api/v2/pokemon/ditto')
+      .then(response => response.json())
+      .then(pockemon => this.setState({ pockemon }))
+      .finally(() => this.setState({ loading: false }));
+  }
+
+  render() {
+    return (
+      <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
+        {this.state.loading && <div>...Завантажуємо!..</div>}
+        {this.state.pockemon && <div>{this.state.pockemon.name}</div>}
+      </div>
+    );
+  }
+}
