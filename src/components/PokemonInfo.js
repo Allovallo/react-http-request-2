582e4bcafb3d86976a241527c73ff702333e03f4
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-06-03 14:50:41 +0300

    files updated

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 export default class PokemonInfo extends Component {
+  state = {
+    pokemon: null,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const prevName = prevProps.pokemonName;
+    const nextName = this.props.pokemonName;
+
+    if (prevName !== nextName) {
+      console.log('Змінилося ім`я покемону!');
+
+      fetch(`http://pokeapi.co/api/v2/pokemon/${nextName}`)
+        .then(response => response.json())
+        .then(pokemon => this.setState({ pokemon: pokemon }));
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>PokemonInfo</h1>
-        <p>{this.props.pokemonName}</p>
+        {!this.props.pokemonName && <div>Введіть ім`я покемону!</div>}
+        {this.state.pokemon && this.state.pokemon.name}
       </div>
     );
   }
